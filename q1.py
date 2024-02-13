4976d757b1009d41dbff877863e39cbbcf0fd94e
Hamzah Mandoo <hmandoo@myseneca.ca>
2024-02-13 12:26:16 -0500

    Added python files for each question as well as markdown file for q5 and associated files for python problems that require text files.

--- q1.py
+++ q1.py
@@ -1,1 +1,57 @@
-def getPrimeNumbers(n):
+
+def checkPrimeNumber(number):
+    """
+    Function to check if the given number is prime or not.
+    Parameters:
+    - number (integer): the parameter inputted to check if its prime or not
+    Returns:
+    - bool: if the number inputted is prime then the function returns true, if it isn't then returns false.
+    """
+    if number == 1:
+        return False
+    for x in range(2, number):
+        if (number % x) == 0:
+            return False
+    return True
+
+def getPrimeNumbers(number):
+    """
+    Function to generate a list of prime numbers between 2 and a given number (number).
+    Parameters:
+    - number (integer): the parameter used to generate the limit of prime numbers.
+    Returns:
+    - list: the function returns a list containing prime numbers between 2 and number.
+    """
+    return [x for x in range(2, number + 1) if checkPrimeNumber(x)]
+    # for each element in range(starts at 2, stops at number + 1 only returns the number if checkPrimeNumber returns true)
+
+def main():
+    print("What number would you like to check prime numbers for: ")
+    number = input()
+    result = getPrimeNumbers(int(number))
+    print("Prime numbers between 2 and " +str(number)+" : " +str(result))
+
+if __name__ == "__main__":
+    main()
+
+# 1.
+# I used a list for collection and returning the collection of prime numbers 
+# because lists are good for iterating or indexing between a list of things such as prime numbers
+
+# 2.    
+# I use a for loop in my function (checkPrimeNumber) because without it the code would have to 
+# slowly and unoptimally go through the list line by line instead of quickly using the for loop
+
+# 3.    
+# I can't avoid a function call here because I am using a helper function to check if a number is prime or not
+# instead of including that logic in my getPrimeNumbers function. I also call getPrimeNumbers in 
+# main which is another function call
+
+# 4.    
+# checkPrimeNumber goes through every number between 2 and the specified 
+# number to check if each number between those numbers are prime.
+
+# getPrimeNumbers generates a list of prime numbers between 2 and the specified number using checkPrimeNumber 
+# to see if the number in the list is prime or not.
+
+# lastly my main function takes input from the user and calls getPrimeNumbers to output the list
--- q3.py
+++ q3.py
@@ -1,1 +1,42 @@
 def wordCount(t):
+    
+	f = open(t, "r") # Open the file specified by the 't' in read mode
+	wordDictionary = dict() # Initialize an empty dictionary to store word counts
+	wordLineCount = 1 # Initialize a counter variable to track the line number 
+	
+	for word in f: # reading each line in the file a for using loop
+		word = word.strip() # Remove leading and trailing whitespace from the line
+		word = word.lower() # Convert the line to lowercase to standardize word case
+		individualWords = word.split(" ")	# Split the line into words			
+
+		for word in individualWords: # Iterate over each individual word
+			if word in wordDictionary: # If: the word is already in the dictionary
+				wordDictionary[word].append(wordLineCount) # If the word is present, append the line number to its list of occurrences
+			else: # Else: the word is not in the dictionary
+				wordDictionary[word] = [] # Create a new list for the word in the dictionary
+				wordDictionary[word].append(wordLineCount) # Append the current line number to the list of occurrences for the word
+		wordLineCount += 1 # Increment the line number counter
+	return wordDictionary # returns a dictionary for the words and their line numbers
+
+def main():
+    print(wordCount("testWord.txt"))
+
+if __name__ == "__main__":
+    main()
+	
+
+# 1.
+# I chose a dictionary data structure because it allows for efficient lookup of words and their associated list of line numbers. 
+# In this case, each word encountered in the file is used as a key in the dictionary, and its associated value is a list of line numbers where the word occurs.
+	
+# 2.
+# I don't think you can avoid a loop here because you need to iterate through the file and make sure all the words don't have trailing spaces, as well as
+# making them all lowercase. Then you need to check if the words are in the dictionary or not already which requires another loop. If there is a library that does this already you can avoid
+# loops by calling functions from that library.
+	
+# 3.
+# We only have 1 function here which is wordCount so I don't think we can avoid this function call unless we put all of our logic in the main function.
+	
+# 4.
+# my for loops go through different parts (first for loop going through the file and then second going through the words individually) so I think we can't avoid repetitive code here unless I use
+# different functions to replace parts
